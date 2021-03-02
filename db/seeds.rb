c070573462a9c1a5fe106b3f762abaabb56e7003
Ubuntu <ubuntu@ip-172-31-18-123.us-east-2.compute.internal>
2021-03-02 15:31:34 +0000

    solved CORS issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-15.times do
+25.times do
 ArtPlace.create({ Location: Faker::Mountain.range,
                   budget: '1600 USD',
                   description: Faker::Lorem.sentence,
-                  urlPic: 'https://unsplash.com/photos/SfPOkp6-2eA?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink'
+                  urlPic: 'https://images.unsplash.com/photo-1552710307-537199cd41c0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
     
                  })
 
