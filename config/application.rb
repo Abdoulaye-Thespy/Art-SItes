cdcda00c8792e1a4fa932d0d37d3e5e282674fec
Ubuntu <ubuntu@ip-172-31-18-123.us-east-2.compute.internal>
2021-03-02 13:14:44 +0000

    adds CORS functionnalities

--- config/application.rb
+++ config/application.rb
@@ -21,10 +21,20 @@ module ArtSites
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  end
+end
   end
 end
