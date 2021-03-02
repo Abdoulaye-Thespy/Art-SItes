d2ad99d534fe6f8e5f2a6ae41d6c8d2a57558931
Ubuntu <ubuntu@ip-172-31-18-123.us-east-2.compute.internal>
2021-03-02 15:40:30 +0000

    CORS issue

--- config/application.rb
+++ config/application.rb
@@ -28,13 +28,5 @@ module ArtSites
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
-    
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
-  end
-end
   end
 end
